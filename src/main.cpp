5d4416cbe7c01d58b6e76dfaa92b7060fb9fa039
Rahul Mitra <rahulm@bu.edu>
2024-05-31 11:35:50 -0700

    currently handling multiple meshes with gizmos, got to figure out a better way to do this

--- src/main.cpp
+++ src/main.cpp
@@ -24,21 +24,14 @@
 using namespace geometrycentral;
 using namespace geometrycentral::surface;
 
-//Let's say for now we're dealing with only 2 patches 
+int numPatches;//number of patches
 
-// == Geometry-central data
-std::unique_ptr<ManifoldSurfaceMesh> mesh1;
-std::unique_ptr<VertexPositionGeometry> geometry1;
+//make a vector to handle all the patches 
+//meshes + geometries 
+std::vector<std::unique_ptr<ManifoldSurfaceMesh>> meshes;
+std::vector<std::unique_ptr<VertexPositionGeometry>> geometries;
+std::vector<polyscope::SurfaceMesh *> psMeshes;
 
-// Polyscope visualization handle, to quickly add data to the surface
-polyscope::SurfaceMesh *psMesh1;
-
-// == Geometry-central data
-std::unique_ptr<ManifoldSurfaceMesh> mesh2;
-std::unique_ptr<VertexPositionGeometry> geometry2;
-
-// Polyscope visualization handle, to quickly add data to the surface
-polyscope::SurfaceMesh *psMesh2;
 
 // Some algorithm parameters
 float param1 = 42.0;
@@ -48,50 +41,52 @@ float param1 = 42.0;
 void showStripePatterns() {
 
 
-  std::vector<Vertex> zeroVertices = getBoundaryVertices(*geometry1, 1).first;
-  std::vector<Vertex> oneVertices = getBoundaryVertices(*geometry1, 1).second;
+  //do this for every patch
+  for (int i = 0; i < numPatches; i++){  
+    std::vector<Vertex> zeroVertices = getBoundaryVertices(*geometries[i], 1).first;
+    std::vector<Vertex> oneVertices = getBoundaryVertices(*geometries[i], 1).second;
 
-  VertexData<double> timeFunction = solveLaplace(*geometry1, zeroVertices, oneVertices);
+    VertexData<double> timeFunction = solveLaplace(*geometries[i], zeroVertices, oneVertices);
 
-  psMesh1 -> addVertexScalarQuantity("time function", timeFunction);
-  FaceData<Vector3> faceGrads = computeTimeFunctionFaceGrad(*geometry1, timeFunction);
-  psMesh1 -> addFaceVectorQuantity("face gradients", faceGrads);
+    psMeshes[i] -> addVertexScalarQuantity("time function_" + std::to_string(i), timeFunction);
+    FaceData<Vector3> faceGrads = computeTimeFunctionFaceGrad(*geometries[i], timeFunction);
+    psMeshes[i] -> addFaceVectorQuantity("face gradients_" + std::to_string(i), faceGrads);
   
 
-  //Compute course stripe patterns
-  VertexData<Vector3> courseVertexValuedField = computeVertexValuedField(*geometry1, timeFunction, 0);
-  psMesh1 -> addVertexVectorQuantity("course vertex valued field", courseVertexValuedField);
-  VertexData<Vector2> lineFieldCourse = vertexDirectionField(*geometry1, courseVertexValuedField);
-  double constantFreq = 40.;
-  VertexData<double> frequencies(*mesh1, constantFreq);
-  CornerData<double> periodicFunc;
-  FaceData<int> zeroIndices;
-  FaceData<int> branchIndices;
-  std::tie(periodicFunc, zeroIndices, branchIndices) =
-      computeStripePattern(*geometry1, frequencies, lineFieldCourse);
+    //Compute course stripe patterns
+    VertexData<Vector3> courseVertexValuedField = computeVertexValuedField(*geometries[i], timeFunction, 0);
+    psMeshes[i] -> addVertexVectorQuantity("course vertex valued field_" + std::to_string(i), courseVertexValuedField);
+    VertexData<Vector2> lineFieldCourse = vertexDirectionField(*geometries[i], courseVertexValuedField);
+    double constantFreq = 40.;
+    VertexData<double> frequencies(*meshes[i], constantFreq);
+    CornerData<double> periodicFunc;
+    FaceData<int> zeroIndices;
+    FaceData<int> branchIndices;
+    std::tie(periodicFunc, zeroIndices, branchIndices) =
+        computeStripePattern(*geometries[i], frequencies, lineFieldCourse);
 
-  // Extract isolines
-  std::vector<Vector3> isolineVerts;
-  std::vector<std::array<size_t, 2>> isolineEdges;
-  std::tie(isolineVerts, isolineEdges) = extractPolylinesFromStripePattern(
-    *geometry1, periodicFunc, zeroIndices, branchIndices, lineFieldCourse, false);
+    // Extract isolines
+    std::vector<Vector3> isolineVerts;
+    std::vector<std::array<size_t, 2>> isolineEdges;
+    std::tie(isolineVerts, isolineEdges) = extractPolylinesFromStripePattern(
+      *geometries[i], periodicFunc, zeroIndices, branchIndices, lineFieldCourse, false);
 
-  polyscope::registerCurveNetwork("course stripe patterns", isolineVerts, isolineEdges);
+    polyscope::registerCurveNetwork("course stripe patterns_" + std::to_string(i), isolineVerts, isolineEdges);
 
 
-  // Compute wale stripe patterns
-  VertexData<Vector3> waleVertexValuedField = computeVertexValuedField(*geometry1, timeFunction, PI/2);
-  psMesh1 -> addVertexVectorQuantity("wale vertex valued field", waleVertexValuedField);
-  VertexData<Vector2> lineFieldWale = vertexDirectionField(*geometry1, waleVertexValuedField);
-  std::tie(periodicFunc, zeroIndices, branchIndices) =
-      computeStripePattern(*geometry1, frequencies, lineFieldWale);
+    // // Compute wale stripe patterns
+    VertexData<Vector3> waleVertexValuedField = computeVertexValuedField(*geometries[i], timeFunction, PI/2);
+    psMeshes[i] -> addVertexVectorQuantity("wale vertex valued field_" + std::to_string(i), waleVertexValuedField);
+    VertexData<Vector2> lineFieldWale = vertexDirectionField(*geometries[i], waleVertexValuedField);
+    std::tie(periodicFunc, zeroIndices, branchIndices) =
+      computeStripePattern(*geometries[i], frequencies, lineFieldWale);
 
-  // Extract isolines
-  std::tie(isolineVerts, isolineEdges) = extractPolylinesFromStripePattern(
-    *geometry1, periodicFunc, zeroIndices, branchIndices, lineFieldWale, false);
+    // Extract isolines
+    std::tie(isolineVerts, isolineEdges) = extractPolylinesFromStripePattern(
+      *geometries[i], periodicFunc, zeroIndices, branchIndices, lineFieldWale, false);
 
-  polyscope::registerCurveNetwork("wale stripe patterns", isolineVerts, isolineEdges);
-
+    polyscope::registerCurveNetwork("wale stripe patterns_" + std::to_string(i), isolineVerts, isolineEdges);
+  }
 }
 
 // A user-defined callback, for creating control panels (etc)
@@ -108,31 +103,27 @@ void callBacks() {
 
 int main(int argc, char **argv) {
 
-  // Initialize polyscope
+  numPatches = argc - 1;
+
+  //resize vectors to account for all the input patches 
+  meshes.resize(numPatches);
+  geometries.resize(numPatches);
+  psMeshes.resize(numPatches);
+
   polyscope::init();
 
+  //populate the meshes/geometries vectors
+  for (int i = 1; i < argc; i++){
+    std::tie(meshes[i - 1], geometries[i - 1]) = readManifoldSurfaceMesh(argv[i]);
+    psMeshes[i - 1] = polyscope::registerSurfaceMesh(
+      polyscope::guessNiceNameFromPath(argv[i]),
+      geometries[i - 1]->inputVertexPositions, meshes[i - 1]->getFaceVertexList(),
+      polyscopePermutations(*meshes[i - 1]));
+  }
+  
   // Set the callback function
   polyscope::state::userCallback = callBacks;
 
-  // Load the first mesh
-  std::tie(mesh1, geometry1) = readManifoldSurfaceMesh(argv[1]);
-
-  // Register the mesh with polyscope
-  psMesh1 = polyscope::registerSurfaceMesh(
-      polyscope::guessNiceNameFromPath(argv[1]),
-      geometry1->inputVertexPositions, mesh1->getFaceVertexList(),
-      polyscopePermutations(*mesh1));
-
-  // Load the second mesh
-  std::tie(mesh2, geometry2) = readManifoldSurfaceMesh(argv[2]);
-
-  // Register the mesh with polyscope
-  psMesh2 = polyscope::registerSurfaceMesh(
-      polyscope::guessNiceNameFromPath(argv[2]),
-      geometry2->inputVertexPositions, mesh2->getFaceVertexList(),
-      polyscopePermutations(*mesh2));
- 
-  // Give control to the polyscope gui
   polyscope::show();
 
   return EXIT_SUCCESS;
