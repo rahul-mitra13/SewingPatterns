139848e3594d89dd3a79838f97be77448d879927
Matteo Couplet <matteo.couplet@gmail.com>
2025-05-26 17:51:32 -0400

    Treat course alignment constraints

--- src/gmsh_helpers.cpp
+++ src/gmsh_helpers.cpp
@@ -3,6 +3,7 @@
 #include "gmsh_helpers.h"
 
 #include "geometrycentral/surface/surface_mesh_factories.h"
+#include "geometrycentral/surface/remeshing.h"
 
 using namespace std;
 
@@ -38,11 +39,25 @@ void parseMsh(
   
     tie(mesh, geometry) = makeManifoldSurfaceMeshAndGeometry(elementNodeTags, vertexPositions);
   
+    // //make the mesh Delaunay 
+    // fixDelaunay(*mesh, *geometry); // we make the mesh approximately Delaunay
+
     // Define map from pair of vertices to edge
     map<pair<int,int>, int> vertexPairToHalfedge;
-    for (Edge e : mesh->edges())
-      vertexPairToHalfedge[{e.firstVertex().getIndex(), e.secondVertex().getIndex()}] = e.getIndex();
-  
+    for (Halfedge he : mesh->halfedges())
+      vertexPairToHalfedge[{he.tailVertex().getIndex(), he.tipVertex().getIndex()}] = he.edge().getIndex();
+      // vertexPairToHalfedge[{e.firstVertex().getIndex(), e.secondVertex().getIndex()}] = e.getIndex();
+    
+    // Get all physical group names
+    vector<pair<int,int>> dimTags;
+    gmsh::model::getPhysicalGroups(dimTags, 1);
+    set<string> physicalGroupNames;
+    for (auto &[dim,tag] : dimTags) {
+      string name;
+      gmsh::model::getPhysicalName(dim, tag, name);
+      physicalGroupNames.insert(name);
+    }
+
     // Get physical groups to find start and end loops
     vector<pair<int,int>> startEntities, endEntities;
     gmsh::model::getEntitiesForPhysicalName("start", startEntities);
@@ -75,6 +90,38 @@ void parseMsh(
     for (int node : uniqueEndNodes)
       globalBdyConditions.courseEndBoundaryVertices.push_back(nodeTagMap[node]);
   
+    // Get physical groups for course alignment
+    if (physicalGroupNames.count("alignCourse")) {
+      vector<pair<int,int>> alignCourseEntities;
+      gmsh::model::getEntitiesForPhysicalName("alignCourse", alignCourseEntities);
+      for (auto &[entDim, entTag] : alignCourseEntities) {
+        vector<int> elementTypes;
+        vector<vector<size_t>> elementTags, elementNodeTags;
+        gmsh::model::mesh::getElements(elementTypes, elementTags, elementNodeTags, entDim, entTag);
+        for (int i = 0; i < elementTags[0].size(); i++) {
+          int nodeTag1 = elementNodeTags[0][2*i], nodeTag2 = elementNodeTags[0][2*i+1];
+          int edge = vertexPairToHalfedge[{nodeTagMap[nodeTag1], nodeTagMap[nodeTag2]}];
+          globalBdyConditions.courseBdyEdges.push_back(edge);
+        }
+      }
+    }
+
+    // Get physical groups for wale alignment (TODO: what to do with edges?)
+    if (physicalGroupNames.count("alignWale")) {
+      vector<pair<int,int>> alignWaleEntities;
+      gmsh::model::getEntitiesForPhysicalName("alignWale", alignWaleEntities);
+      for (auto &[entDim, entTag] : alignWaleEntities) {
+        vector<int> elementTypes;
+        vector<vector<size_t>> elementTags, elementNodeTags;
+        gmsh::model::mesh::getElements(elementTypes, elementTags, elementNodeTags, entDim, entTag);
+        for (int i = 0; i < elementTags[0].size(); i++) {
+          int nodeTag1 = elementNodeTags[0][2*i], nodeTag2 = elementNodeTags[0][2*i+1];
+          int edge = vertexPairToHalfedge[{nodeTagMap[nodeTag1], nodeTagMap[nodeTag2]}];
+          // globalBdyConditions.courseBdyEdges.push_back(edge);
+        }
+      }
+    }
+
     H(globalBdyConditions.courseStartBoundaryVertices.size());
     H(globalBdyConditions.courseEndBoundaryVertices.size());
     H(globalBdyConditions.courseBdyEdges.size());
