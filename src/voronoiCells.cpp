be60e728a6d5b6b5e0b5fedabe88c1a6f01467d3
Rahul Mitra <rahulm@bu.edu>
2025-07-01 18:22:36 -0400

    minor

--- src/voronoiCells.cpp
+++ src/voronoiCells.cpp
@@ -149,7 +149,7 @@ VoronoiResult computeGeodesicCentroidalVoronoiTessellationWithWeights(SurfaceMes
     // UPDATE WEIGHTS WITH FIXED SITES (using Karcher mean)
 
     //double alpha = 2, beta = 0.1;
-    double alpha = 2, beta = 0.; // standard gradient descent 
+    double alpha = 1e-3, beta = 0.; // standard gradient descent 
 
     // Nesterov acceleration
     vector<double> phiWeightsY(nSites);
