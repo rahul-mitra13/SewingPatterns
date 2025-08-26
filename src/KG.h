159975435e639bdbaa06927e4afb5a38c6fceb75
Rahul Mitra <rahulm@bu.edu>
2025-08-26 17:36:10 -0400

    add new knit graph file

--- src/KG.h
+++ src/KG.h
@@ -23,21 +23,18 @@ using namespace geometrycentral::surface;
 //redefining for consistency
 struct KGVertex{
     int id = -1;
-    Vector3 position;//position embedded in R^3 (if position information is available)
-    Vector3 baryCoords;//barycentric coordinates of the vertex if position information is not available
+    Vector3* position;//position embedded in R^3 (if position information is available)
+    Vector3* baryCoords;//barycentric coordinates of the vertex if position information is not available
     int row_in = -1;
     int row_out = -1;
     int col_in[2] = {-1, -1};
     int col_out[2] = {-1, -1};
     double alpha_tag = -1;
     double beta_tag = -1;
-    Face face;//associated face of a vertex (on the underlying mesh)
-    std::optional<Edge> edge;//associated edge of a vertex
-    std::optional<Halfedge> halfedge;//associated halfedge of a vertex
-    SurfacePoint surfacePoint;//used for intersections on singular faces
-    bool isBaryCenter = false;//if this is a vertex at the barycenter
-    bool hasBeenHandled = false;//if this vertex has been handled by a merge
-    bool isVirtual = false;//if this is a virtual vertex
+    Face* face;//associated face of a vertex (on the underlying mesh) (for real vertices)
+    std::optional<Edge> edge;//associated edge of a vertex (for virtual vertices)
+    std::optional<Halfedge> halfedge;//associated halfedge of a vertex (for virtual vertices)
+    SurfacePoint surfacePoint;//surface point representation of this vertex
     bool isAlphaVirtual = false;//is a virtual vertex in the course direction
     bool isBetaVirtual = false;//is a virtual vertex in the wale direction 
 
@@ -50,6 +47,7 @@ struct KGVertex{
         std::cout << "col_in[1] = " << col_in[1] << std::endl;
         std::cout << "col_out[0] = " << col_out[0] << std::endl;
         std::cout << "col_out[1] = " << col_out[1] << std::endl;
-        std::cout << "isVirtual = " << isVirtual << std::endl;
+        std::cout << "isAlphaVirtual = " << isAlphaVirtual << std::endl;
+        std::cout << "isBetaVirtual = " << isBetaVirtual << std::endl;
     }
 };
