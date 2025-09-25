8947faf27da7002f6171104c9b914a0fba63d0c6
Rahul Mitra <rahulm@bu.edu>
2025-09-24 21:26:38 -0400

    duck = scheduled

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,7 @@ void showStripePatterns(){
   // polyscope::show();
 
   std::string richDataFile;
-  // richDataFile = "pipes_info_0.125.ply";
+  //richDataFile = "ducK_info_0.02.ply";
 
   //-------iteratively find course stripes and course singularities----------//
   CornerData<double> courseStripeValues(globalGeometry -> mesh);
